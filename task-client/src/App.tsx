cecc3d2065e34f9d78c14ed8229f1af147e0e683
FlyingCat2703 <hanlyngoc1204@gmail.com>
2025-11-12 16:42:53 +0700

    Kanban layout

--- task-client/src/App.tsx
+++ task-client/src/App.tsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
-import TaskList from './pages/TaskList/TaskList';
+import TaskBoard from './pages/TaskBoard/TaskBoard';
 
 function App() {
   return (
     <>
       <Routes>
         <Route element={ <Layout/> }>
-          <Route path='/' element={ <TaskList/> } />
+          <Route path='/' element={ <TaskBoard/> } />
         </Route>
       </Routes>
     </>
